b7f6b9518da7355d82e50d7939320267a7525bd3
James Margenberg <james@margenberg.com.au>
2019-05-18 20:39:03 +1000

    Added selection of multiple participants.

--- client/src/components/FilterList/index.js
+++ client/src/components/FilterList/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Select from "react-select"; 
+import Select from "react-select";
 
 
-export function FilterList ({handleChange, options}) {
-  console.log(options); 
-
-
-    return (    <Select options = {options} onChange= {v => handleChange(v.value)}/>
-
-        );
+export function FilterList({ handleChange, options, isMulti = false }) {
+  return (
+    <Select options={options} onChange={v => handleChange(v.value)} isMulti={isMulti} />
+  );
 
 }
 
