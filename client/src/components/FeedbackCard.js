85c20664f3cc1b125632c93f892f9bf6f0fbc036
James Margenberg <james@margenberg.com.au>
2019-05-18 21:05:45 +1000

    Applied basic styling to feedback card.

--- client/src/components/FeedbackCard.js
+++ client/src/components/FeedbackCard.js
@@ -6,27 +6,31 @@ export function FeedbackCard({
 }) {
 
     return (
-        <div>
-            <h3>Participant Question (Optional)</h3>
-            <IntensitySelector
-                id='participant.happiness'
-                handleSelect={handleChange('0')}
-                label="How happy were you with today’s program? "
-            />
-            <h3>Trainer Questions</h3>
-            <IntensitySelector
-                id='trainer.participantEngagement'
-                handleSelect={handleChange('0')}
-                label="How engaged was the participant in the program?"
-            />
+        <div className="feedback-card">
+            <div className="feedback-card__participant-name"> Name of Participant</div>
+            <div className="question-group">
+                <div className="question-group__title"> Participant Question (Optional)</div>
+                <IntensitySelector
+                    id='participant.happiness'
+                    handleSelect={handleChange('0')}
+                    label="How happy were you with today’s program? "
+                />
+            </div>
+            <div className="question-group">
+                <div className="question-group__title">Trainer Questions</div>
+                <IntensitySelector
+                    id='trainer.participantEngagement'
+                    handleSelect={handleChange('0')}
+                    label="How engaged was the participant in the program?"
+                />
 
-            <IntensitySelector
-                id='trainer.participantHappiness'
-                handleSelect={handleChange('1')}
-                label="How happy was the participant with today’s program?"
-            />
+                <IntensitySelector
+                    id='trainer.participantHappiness'
+                    handleSelect={handleChange('1')}
+                    label="How happy was the participant with today’s program?"
+                />
+            </div>
         </div>
     );
-
 }
 
